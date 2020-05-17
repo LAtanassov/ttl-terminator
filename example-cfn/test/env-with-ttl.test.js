ff2a3d4abbdd9b6bbb7af577c7b58738e26aed98
LAtanassov <latschesar.atanassov@gmx.at>
2020-05-17 09:27:59 +0200

    Add unit test with TTL tag

--- example-cfn/test/env-with-ttl.test.js
+++ example-cfn/test/env-with-ttl.test.js
@@ -1,16 +1,25 @@
 const { expect, matchTemplate, MatchStyle } = require('@aws-cdk/assert');
-const cdk = require('@aws-cdk/core');
+const { App, Tag } = require('@aws-cdk/core');
 const EnvWithTtl = require('../lib/env-with-ttl-stack');
 
 test('CFN Stack with one bucket and TTL tag', () => {
-    const app = new cdk.App();
+    const app = new App();
     // WHEN
     const stack = new EnvWithTtl.EnvWithTtlStack(app, 'MyTestStack');
+    Tag.add(stack, 'TTL', new Date(Date.parse("2020-01-15T23:15:23.991Z")).toISOString());
     // THEN
     expect(stack).to(matchTemplate({
       "Resources": {
         "bucketwithttlB72D1F93": {
           "Type": "AWS::S3::Bucket",
+          "Properties": {
+            "Tags": [
+              {
+                "Key": "TTL",
+                "Value": "2020-01-15T23:15:23.991Z"
+              }
+            ]
+          },
           "UpdateReplacePolicy": "Retain",
           "DeletionPolicy": "Retain"
         }
